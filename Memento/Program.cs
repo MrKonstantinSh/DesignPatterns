502d61cbc19d7d7f005c6f83d3408be42d660bb3
Shulga Konstantin <mrkonstantinsh@gmail.com>
2020-10-29 12:06:32 +0300

    chore(memento): add project files

---
+++ Memento/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Memento
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
